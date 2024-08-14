5e229b63e5adf2818c37f004d50b3f1c7f9cfbc6
lily <174732028+lily4web@users.noreply.github.com>
2024-08-14 15:41:14 -0400

    Add half of columns and rows

---
+++ script.js
@@ -0,0 +1,178 @@
+const container = document.createElement("div");
+const body = document.querySelector("body");
+const div = document.createElement("div");
+container.classList.add("container");
+const container_one = document.createElement("div");
+body.append(container)
+
+container.append(container_one)
+container_one.classList.add("container_one");
+
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
+const div3 = document.createElement("div");
+const div4 = document.createElement("div");
+const div5 = document.createElement("div");
+const div6 = document.createElement("div");
+const div7 = document.createElement("div");
+const div8 = document.createElement("div");
+const div9 = document.createElement("div");
+const div10 = document.createElement("div");
+const div11 = document.createElement("div");
+const div12 = document.createElement("div");
+const div13 = document.createElement("div");
+const div14 = document.createElement("div");
+const div15 = document.createElement("div");
+const div16 = document.createElement("div");
+
+container_one.append(div1);
+container_one.append(div2);
+container_one.append(div3);
+container_one.append(div4);
+container_one.append(div5);
+container_one.append(div6);
+container_one.append(div7);
+container_one.append(div8);
+container_one.append(div9);
+container_one.append(div10);
+container_one.append(div11);
+container_one.append(div12);
+container_one.append(div13);
+container_one.append(div14);
+container_one.append(div15);
+container_one.append(div16);
+
+div1.classList.add("squares");
+div2.classList.add("squares");
+div3.classList.add("squares");
+div4.classList.add("squares");
+div5.classList.add("squares");
+div6.classList.add("squares");
+div7.classList.add("squares");
+div8.classList.add("squares");
+div9.classList.add("squares");
+div10.classList.add("squares");
+div11.classList.add("squares");
+div12.classList.add("squares");
+div13.classList.add("squares");
+div14.classList.add("squares");
+div15.classList.add("squares");
+div16.classList.add("squares");
+
+const container_two = document.createElement("div");
+container.append(container_two)
+container_two.classList.add("container_two");
+
+const div17 = document.createElement("div");
+const div18 = document.createElement("div");
+const div19 = document.createElement("div");
+const div20 = document.createElement("div");
+const div21 = document.createElement("div");
+const div22 = document.createElement("div");
+const div23 = document.createElement("div");
+const div24 = document.createElement("div");
+const div25 = document.createElement("div");
+const div26 = document.createElement("div");
+const div27 = document.createElement("div");
+const div28 = document.createElement("div");
+const div29 = document.createElement("div");
+const div30 = document.createElement("div");
+const div31 = document.createElement("div");
+const div32 = document.createElement("div");
+
+container_two.append(div17, div18, div19, div20, div20, div21, div22, div23, div24, div25, div26, div27, div28, div29, div30, div31, div32);
+
+
+div17.classList.add("squares"), div18.classList.add("squares"), div19.classList.add("squares"), div20.classList.add("squares"), div21.classList.add("squares"),div22.classList.add("squares")
+div23.classList.add("squares")
+div24.classList.add("squares")
+div25.classList.add("squares")
+div26.classList.add("squares")
+div27.classList.add("squares")
+div28.classList.add("squares")
+div29.classList.add("squares")
+div30.classList.add("squares")
+div31.classList.add("squares")
+div32.classList.add("squares");
+
+const container_three = document.createElement("div");
+container.append(container_three)
+container_three.classList.add("container_three");
+
+const div33 = document.createElement("div"), div34 = document.createElement("div"), div35 = document.createElement("div"), div36 = document.createElement("div"), div37 = document.createElement("div"), div38 = document.createElement("div"), div39 = document.createElement("div"), div40 = document.createElement("div"), div41 = document.createElement("div"), div42 = document.createElement("div"), div43 = document.createElement("div"), div44 = document.createElement("div"), div45 = document.createElement("div"), div46 = document.createElement("div"), div47 = document.createElement("div"), div48 = document.createElement("div");
+
+container_three.append(div33, div34, div35, div36, div37, div38, div39, div40, div41, div42, div43, div44, div45, div46, div47, div48);
+
+div33.classList.add("squares");
+div34.classList.add("squares");
+div35.classList.add("squares");
+div36.classList.add("squares");
+div37.classList.add("squares");
+div38.classList.add("squares");
+div39.classList.add("squares");
+div40.classList.add("squares");
+div41.classList.add("squares");
+div42.classList.add("squares");
+div43.classList.add("squares");
+div44.classList.add("squares");
+div45.classList.add("squares");
+div46.classList.add("squares");
+div47.classList.add("squares");
+div32.classList.add("squares");
+
+
+
+const container_four = document.createElement("div");
+container.append(container_four)
+container_four.classList.add("container_four");
+
+const div49 = document.createElement("div"), div50 = document.createElement("div"), div51 = document.createElement("div"), div52 = document.createElement("div"), div53 = document.createElement("div"), div54 = document.createElement("div"), div55 = document.createElement("div"), div56 = document.createElement("div"), div57 = document.createElement("div"), div58 = document.createElement("div"), div59 = document.createElement("div"), div60 = document.createElement("div"), div61 = document.createElement("div"), div62 = document.createElement("div"), div63 = document.createElement("div"), div64 = document.createElement("div");
+
+container_four.append(div49, div50, div51, div52, div53, div54, div55, div56, div57, div58, div59, div60, div61, div62, div63, div64);
+
+div49.classList.add("squares"), div50.classList.add("squares"), div51.classList.add("squares"), div52.classList.add("squares"), div53.classList.add("squares"), div54.classList.add("squares"), div55.classList.add("squares"), div56.classList.add("squares"), div57.classList.add("squares"), div58.classList.add("squares"), div59.classList.add("squares"), div60.classList.add("squares"), div61.classList.add("squares"), div62.classList.add("squares"), div63.classList.add("squares"), div64.classList.add("squares");
+
+
+const container_five = document.createElement("div");
+container.append(container_five)
+container_five.classList.add("container_five");
+
+const div65 = document.createElement("div"), div66 = document.createElement("div"), div67 = document.createElement("div"), div68 = document.createElement("div"), div69 = document.createElement("div"), div70 = document.createElement("div"), div71 = document.createElement("div"), div72 = document.createElement("div"), div73 = document.createElement("div"), div74 = document.createElement("div"), div75 = document.createElement("div"), div76 = document.createElement("div"), div77 = document.createElement("div"), div78 = document.createElement("div"), div79 = document.createElement("div"), div80 = document.createElement("div");
+
+container_five.append(div65, div66, div67, div68, div69, div70, div71, div72, div73, div74, div75, div76, div77, div78, div79, div80);
+
+div49.classList.add("squares"), div65.classList.add("squares"), div66.classList.add("squares"), div67.classList.add("squares"), div68.classList.add("squares"), div69.classList.add("squares"), div70.classList.add("squares"), div71.classList.add("squares"), div72.classList.add("squares"), div73.classList.add("squares"), div74.classList.add("squares"), div75.classList.add("squares"), div76.classList.add("squares"), div77.classList.add("squares"), div78.classList.add("squares"), div79.classList.add("squares"), div80.classList.add("squares");
+
+
+const container_six = document.createElement("div");
+container.append(container_six)
+container_six.classList.add("container_six");
+
+const div81 = document.createElement("div"), div82 = document.createElement("div"), div83 = document.createElement("div"), div84 = document.createElement("div"), div85 = document.createElement("div"), div86 = document.createElement("div"), div87 = document.createElement("div"), div88 = document.createElement("div"), div89 = document.createElement("div"), div90 = document.createElement("div"), div91 = document.createElement("div"), div92 = document.createElement("div"), div93 = document.createElement("div"), div94 = document.createElement("div"), div95 = document.createElement("div"), div96 = document.createElement("div");
+
+container_six.append(div81, div82, div83, div84, div85, div85, div86, div87, div88, div89, div90, div91, div92, div93, div94, div95, div96);
+
+div49.classList.add("squares"), div81.classList.add("squares"), div82.classList.add("squares"), div83.classList.add("squares"), div84.classList.add("squares"), div85.classList.add("squares"), div86.classList.add("squares"), div87.classList.add("squares"), div88.classList.add("squares"), div89.classList.add("squares"), div90.classList.add("squares"), div91.classList.add("squares"), div92.classList.add("squares"), div93.classList.add("squares"), div94.classList.add("squares"), div95.classList.add("squares"), div96.classList.add("squares");
+
+
+const container_seven = document.createElement("div");
+container.append(container_seven)
+container_seven.classList.add("container_seven");
+
+const div97 = document.createElement("div"), div98 = document.createElement("div"), div99 = document.createElement("div"), div100 = document.createElement("div"), div101 = document.createElement("div"), div102 = document.createElement("div"), div103 = document.createElement("div"), div104 = document.createElement("div"), div105 = document.createElement("div"), div106 = document.createElement("div"), div107 = document.createElement("div"), div108 = document.createElement("div"), div109 = document.createElement("div"), div110 = document.createElement("div"), div111 = document.createElement("div"),div112 = document.createElement("div");
+
+container_seven.append(div97, div98, div99, div100, div101, div102, div103, div104, div105, div106, div107, div108, div109, div110, div111, div112);
+
+div49.classList.add("squares"), div97.classList.add("squares"), div98.classList.add("squares"), div99.classList.add("squares"), div100.classList.add("squares"), div101.classList.add("squares"), div103.classList.add("squares"), div104.classList.add("squares"), div105.classList.add("squares"), div106.classList.add("squares"), div107.classList.add("squares"), div108.classList.add("squares"), div109.classList.add("squares"), div110.classList.add("squares"), div111.classList.add("squares"), div112.classList.add("squares");
+
+const container_eight = document.createElement("div");
+container.append(container_eight)
+container_eight.classList.add("container_eight");
+
+const div113 = document.createElement("div"), div114 = document.createElement("div"), div115 = document.createElement("div"), div116 = document.createElement("div"), div117 = document.createElement("div"), div118 = document.createElement("div"), div119 = document.createElement("div"), div120 = document.createElement("div"), div121 = document.createElement("div"), div122 = document.createElement("div"), div123 = document.createElement("div"), div124 = document.createElement("div"), div125 = document.createElement("div"), div126 = document.createElement("div"), div127 = document.createElement("div"),div128 = document.createElement("div");
+
+container_eight.append(div113, div114, div115, div116, div117, div118, div119, div120, div121, div122, div122, div123, div124, div125, div126, div127, div128);
+
+div113.classList.add("squares"), div114.classList.add("squares"), div115.classList.add("squares"), div116.classList.add("squares"), div117.classList.add("squares"), div118.classList.add("squares"), div119.classList.add("squares"), div120.classList.add("squares"), div121.classList.add("squares"), div121.classList.add("squares"), div122.classList.add("squares"), div123.classList.add("squares"), div124.classList.add("squares"), div125.classList.add("squares"), div126.classList.add("squares"), div127.classList.add("squares"), div128.classList.add("squares");
+
+
